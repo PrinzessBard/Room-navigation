e3b050f0c75f56eb3a6c8b1cf5a5c3c74ab9b8c0
PrinzessBard <egorholikov234@gmail.com>
2024-10-15 22:23:31 +0300

    Test update

--- Test/test.py
+++ Test/test.py
@@ -1,49 +1,20 @@
-coordinates = {}
-with open("text1.txt", encoding="utf-8") as file:
-    for line in file:
-        key, *value = line.split()
-        coordinates[key] = value
-
-new_coordinates = {}
-for key, item in coordinates.items():
-    new_key = int(key)
-    new_coordinates[new_key] = [[int(item[0]), int(item[1])], [int(item[2]), int(item[3])], [int(item[4]), int(item[5])], [int(item[6]), int(item[7])]]
+def similar(first, second):
+    first = first.replace(' ', '')
+    second = second.replace(' ', '')
+    if len(first) - sum(l1==l2 for l1, l2 in zip(first, second)) > 3:
+        return False
+    return True
 
 
-
-for key, item in new_coordinates.items():
-    print(item)
-    for i in item:
-        print(i) # (5, 10)
-        if i[0] == 0 and i[1] == 0:
-            print(i[0], "   ", i[1])
-            item.remove(i) 
+def check_list_dir(list_dir, address):
+    for i in list_dir:
+        if similar(address, i) == True:
+            return i
         else:
-            pass
+            continue
+    return False
 
 
-for key, item in new_coordinates.items():
-    print(item)
-    for i in item:
-        print(i) # (5, 10)
-        if i[0] == 0 and i[1] == 0:
-            print(i[0], "   ", i[1])
-            item.remove(i) 
-        else:
-            pass
+list_dir = ['school_3', 'Bolkhov, Apuhtina']
 
- 
-return new_coordinates
-
-# {'1:': ['[(5,', '10)]'], '2:': ['[(6,', '10)]']}
-# {1: (5, 10), 2: (6, 10)}
-# {1: [(5, 10), (0, 0), (0, 0), (0, 0)]}
-
-# (5, 10)
-# (0, 0)
-# (0, 0)
-# (0, 0)
-# (6, 10)
-# (0, 0)
-# (0, 0)
-# (0, 0)
+print(check_list_dir(list_dir, 'Bolkhov, Apuhtin'))
